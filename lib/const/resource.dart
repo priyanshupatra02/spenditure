0f453878d2133e33a3c0225631092d3f0c2b5e04
Priyanshu Patra <85840449+priyanshupatra02@users.noreply.github.com>
2024-05-18 18:05:47 +0530

    🍱 Updated splash screen

--- lib/const/resource.dart
+++ lib/const/resource.dart
@@ -4,6 +4,14 @@
 class R {
   const R._();
 
+  /// ![preview](file:///Users/priyanshupatra/Desktop/Developer/Flutter Resources & Projects/MacBook/flutter projects/spenditure/assets/icons/spenditure_logo.svg)
+  static const String ASSETS_ICONS_SPENDITURE_LOGO_SVG =
+      'assets/icons/spenditure_logo.svg';
+
   /// ![preview](file:///Users/priyanshupatra/Desktop/Developer/Flutter Resources & Projects/MacBook/flutter projects/spenditure/assets/images/stat-bg.svg)
   static const String ASSETS_IMAGES_STAT_BG_SVG = 'assets/images/stat-bg.svg';
+
+  /// ![preview](file:///Users/priyanshupatra/Desktop/Developer/Flutter Resources & Projects/MacBook/flutter projects/spenditure/assets/logo/ic_launcher_big.png)
+  static const String ASSETS_LOGO_IC_LAUNCHER_BIG_PNG =
+      'assets/logo/ic_launcher_big.png';
 }
--- lib/features/splash/view/splash_view.dart
+++ lib/features/splash/view/splash_view.dart
@@ -1,9 +1,15 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:spenditure/bootstrap.dart';
+import 'package:spenditure/const/color/app_colors.dart';
+import 'package:spenditure/const/resource.dart';
 import 'package:spenditure/features/splash/controller/future_initializer.dart';
 import 'package:spenditure/shared/riverpod_ext/asynvalue_easy_when.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 ///This view displayed for initializing all the required things on initialization.
 /// This will help for initial loading screen for apps with heavy things initialization;
@@ -115,34 +121,70 @@ class _LoaderChildState extends State<LoaderChild> with TickerProviderStateMixin
       left: false,
       right: false,
       child: Container(
-        color: Colors.white,
-        child: Stack(
-          alignment: AlignmentDirectional.center,
+        //add gradient
+        decoration: const BoxDecoration(color: AppColors.kBackgroundColor
+            // gradient: LinearGradient(
+            //   begin: Alignment.topLeft,
+            //   end: Alignment.bottomRight,
+            //   stops: [0.0, 1.0],
+            //   colors: [
+            //     Color(0xFFEEE5FF),
+            //     Color(0xFF9A66FF),
+            //   ],
+            // ),
+            ),
+        child: Column(
+          // alignment: AlignmentDirectional.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Center(
-              child: RotationTransition(
-                turns: _animation,
-                child: const FlutterLogo(
-                  size: 100,
-                ),
+              child: SvgPicture.asset(
+                R.ASSETS_ICONS_SPENDITURE_LOGO_SVG,
+                width: 100,
+                height: 100,
               ),
             ),
-            const Positioned(
-              bottom: 44,
-              child: CircularProgressIndicator(
-                color: Colors.amber,
+            10.heightBox,
+            // Positioned(
+            //   bottom: 16,
+            //   child: Text(
+            //     "Spenditure".toUpperCase(),
+            //     style: const TextStyle(
+            //       fontWeight: FontWeight.bold,
+            //     ),
+            //   ),
+            // ),
+            30.heightBox,
+            // const Positioned(
+            //   bottom: 44,
+            //   child: CircularProgressIndicator(
+            //     color: Colors.amber,
+            //   ),
+            // ),
+            Material(
+              color: Colors.transparent,
+              child: AnimatedTextKit(
+                animatedTexts: [
+                  ColorizeAnimatedText(
+                    'Spenditure.',
+                    colors: [
+                      AppColors.kSecondaryColor,
+                      const Color(0xFF9a66ff),
+                    ],
+                    textStyle: GoogleFonts.inter(
+                      fontSize: 50,
+                      // color:
+                      fontWeight: FontWeight.bold,
+                    ),
+                    // speed: const Duration(milliseconds: 170),
+                  ),
+                ],
+                totalRepeatCount: 4,
+                pause: const Duration(milliseconds: 1000),
+                displayFullTextOnTap: true,
+                stopPauseOnTap: true,
               ),
-            ),
-            const Positioned(
-              bottom: 16,
-              child: Material(
-                  child: Text(
-                "Welcome to Riverpod Simple Architecture App",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              )),
-            ),
+            )
           ],
         ),
       ),
